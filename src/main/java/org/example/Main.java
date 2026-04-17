ce6d44e4a2136fbd552b081cb0a6368d66ccc70f
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-04-17 09:10:13 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,51 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";
+        String usuario = "RIBERA";
+        String contraseña = "ribera";
+
+        //Tabla DEPARTAMENTO
+        try (Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+             Statement statement = conn.createStatement()) {
+
+            try { statement.executeUpdate("DROP TABLE EMPLEADO");
+            } catch (SQLException e) {
+            }
+            try { statement.executeUpdate("DROP TABLE DEPARTAMENTO"); } catch (SQLException e) {
+            }
+
+            String sql = "CREATE TABLE DEPARTAMENTO (" +
+                    "id NUMBER PRIMARY KEY, " +
+                    "nombre VARCHAR2(100))";
+
+            statement.executeUpdate(sql);
+            System.out.println("Tabla 'DEPARTAMENTO' creada");
+
+        } catch (SQLException e) {
+            System.out.println("Error al crear la tabla DEPARTAMENTO: " + e.getMessage());
+        }
+
+        // Tabla EMPLEADO
+        try (Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+             Statement statement = conn.createStatement()) {
+
+            String sql = "CREATE TABLE EMPLEADO (" +
+                    "id NUMBER PRIMARY KEY, " +
+                    "nombre VARCHAR2(100), " +
+                    "salario NUMBER(10, 2), " +
+                    "departamento_id NUMBER, " +
+                    "CONSTRAINT fk_departamento FOREIGN KEY (departamento_id) REFERENCES departamento(id))";
+
+            statement.executeUpdate(sql);
+            System.out.println("Tabla 'EMPLEADO' creada");
+
+        } catch (SQLException e) {
+            System.out.println("Error al crear la tabla EMPLEADO: " + e.getMessage());
         }
     }
 }
